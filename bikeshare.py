ff9f8afc788aa8d0dc981b6acb5c5fae22026bd0
Brent Crabb <Brent.Crabb@turner.com>
2020-09-30 22:12:06 -0400

    Changed welcome statement

--- bikeshare.py
+++ bikeshare.py
@@ -17,7 +17,8 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('\nHello! Let\'s explore some US bikeshare data!\n')
+    # welcome message
+	print('\nHello! Let\'s explore some US bikeshare data!\n')
     
     # get user input for city (chicago, new york city, washington).
     # see readme.txt -- referenced https://wiki.python.org/moin/WhileLoop for help on while loops
